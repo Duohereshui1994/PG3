cf4700066c82407a5d4afef5808ab8a49629ac4c
GC2A_04_ゴ_ウ <147117585+Duohereshui1994@users.noreply.github.com>
2024-07-15 14:17:17 +0900

    08_01_確認課題

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <string>
+#include <chrono>
 
-using namespace std;
+auto CopyTime(const std::string& str) {
+	//開始時間
+	auto startTime = std::chrono::high_resolution_clock::now();
+	//内容をコピーする
+	std::string copy = str;
+	//終了時間
+	auto endTime = std::chrono::high_resolution_clock::now();
+	//時間計算
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+}
+
+auto MoveTime(std::string&& str) {
+	//開始時間
+	auto startTime = std::chrono::high_resolution_clock::now();
+	//内容を移動する
+	std::string move = std::move(str);
+	//終了時間
+	auto endTime = std::chrono::high_resolution_clock::now();
+	//時間計算
+	return std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+}
 
 int main() {
+
+	//文字列を作成した
+	std::string a(10000000, 'a');
+	//コピーでかかった時間
+	auto copyTime = CopyTime(a);
+	//移動でかかった時間
+	auto moveTime = MoveTime(std::move(a));
+
+	//出力
+	std::cout << "文字を移動とコピーで比較しました。" << std::endl;
+
+	std::cout << "コピー：" << copyTime.count() << "μs" << std::endl;
+
+	std::cout << "移動：" << moveTime.count() << "μs" << std::endl;
 
 	return 0;
 }
